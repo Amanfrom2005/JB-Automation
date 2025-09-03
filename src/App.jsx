8428143b438d8f85ec14504be521fcf2974974f9
AmanFrom2005 <masterfrom2005@gmail.com>
2025-09-03 15:03:44 +0530

    working join us page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
+import CareerPage from "./pages/CareerPage";
 import Footer from "./components/Footer";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/career" element={<CareerPage />} />
       </Routes>
       <Footer />
     </>
